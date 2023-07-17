fc04d84f7ced178ce7c083cd711462a6a0ccb11c
WALID ABDUL HAKIM <92942235+walidabdulhakim@users.noreply.github.com>
2023-07-17 20:16:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["New Graduate","Frontend Developer","Computer Engineer","Software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDeplay: 1000,
+    loop:true
+
+})
